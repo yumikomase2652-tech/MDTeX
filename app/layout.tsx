ee3205097cdddab8c9a911bf21a54d61feaa23e0
yumikomase2652-tech <yumiko.mase.2652@gmail.com>
2026-06-11 10:08:54 +0900

    add icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,10 +12,11 @@ export const metadata: Metadata = {
   applicationName: "MDTeX",
   icons: {
     icon: [
-      { url: "/favicon.ico" },
       { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
       { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
+      { url: "/favicon.ico", type: "image/x-icon" },
     ],
+    shortcut: [{ url: "/favicon.ico", type: "image/x-icon" }],
     apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
   },
   appleWebApp: {
